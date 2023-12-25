2d27197f069e3debded834201be6cb87d37dc44e
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-25 22:17:21 +0530

    commit14

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
+
+
+class StackLayoutExample(StackLayout):
+    pass
 
 class GridLayoutExample(GridLayout):
     pass
