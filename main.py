b25ebaf5fb57312b294dc0b6c94073652736ee97
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-25 20:49:08 +0530

    commit8

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.widget import Widget
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
 
 class BoxLayoutExample(BoxLayout):
     pass
