e76e30669d3c6649050c9a459f1a92503c784209
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-25 19:47:48 +0530

    commit4

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+
+class BoxLayoutExample(BoxLayout):
+    pass
 
 
 class MainWidget(Widget):
     pass
+
+
+
 
 class TheLabApp(App):
     pass
