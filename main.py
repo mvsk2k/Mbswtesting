fe941d5d607e6e1e5cd881abfafa1eade7ce3ea0
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-26 20:14:56 +0530

    Button Widget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
@@ -8,6 +9,12 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.pagelayout import PageLayout
 from kivy.uix.widget import Widget
+
+
+class WidgetsExample(GridLayout):
+    my_text = StringProperty('Hello !')
+    def on_button_click(self):
+        self.my_text = "You Clicked !"
 
 
 class PageLayoutExample(PageLayout):
