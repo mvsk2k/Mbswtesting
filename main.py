1c4ce8d5637c487bab708c0894d6b874b2880948
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-30 12:34:50 +0530

    canvas4

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from kivy.app import App
+from kivy.graphics.vertex_instructions import Line, Rectangle
+from kivy.graphics.context_instructions import Color
 from kivy.metrics import dp
 from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
@@ -71,6 +73,16 @@ class CanvasExample2(Widget):
 
 class CanvasExample3(Widget):
     pass
+
+class CanvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100, 100, 400, 500), width=2)
+            Color(0, 1, 0)
+            Line(circle=(400, 200, 80), width=2)
+            Line(rectangle=(700, 500, 150, 100), width=5)
+            Rectangle(pos=(700, 200), size=(150, 100))
 
 
 class MainWidget(Widget):
